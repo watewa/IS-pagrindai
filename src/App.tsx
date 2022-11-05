b1d0a40e8c829ee666cc21aa34f2ad03a10b22ff
Zeptosec <armandasmatkevicius@gmail.com>
2022-11-05 18:45:52 +0200

    Removed a bunch of stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
-export default () => {
 
+const Home = () => {
+    
     return (
         <div className="home">
             Home...
         </div>
     )
 }
+export default Home;
