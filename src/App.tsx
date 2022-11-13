650a8af4d19ca565ffb9939f3c6877179cb26004
Jonas <jonas.liubinavicius@gmail.com>
2022-11-13 21:08:03 +0200

    uzsakymu front

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Signup from './pages/Signup';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Worker from './pages/Worker';
+import Order from './pages/Order'
 
 function App() {
   //const { user } = useAuthContext();
@@ -16,6 +17,7 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path='/order' element={<Order />} />
             <Route path='/worker' element={<Worker />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
