003db38f9a4299990d8ba8f7b6fa811983e4ed7b
vejspa <v.spakauskas@gmail.com>
2022-11-15 18:28:43 +0200

    opa

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,27 +1,37 @@
 const Store = () => {
-    
-
     return (
         <div>
             <h2>Parduotuvių valdymo skydas</h2>
-            <table className="m-auto">
-                <tr>
-                    <th>Nr</th>
-                    <th>Parduotuvės pavadinimas</th>
-                    <th>Parduotuvės adresas</th>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>Centrinė</td> 
-                    <td>Gričiupio g. 1</td> 
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Vakarinė</td> 
-                    <td>Karaliaus Mindaugo pr. 1</td> 
-                </tr>
-            </table>
+            <div className= "container">
+                <table id = "table">
+                    <tr>
+                        <th>Nr</th>
+                        <th>Parduotuvės pavadinimas</th>
+                        <th>Parduotuvės adresas</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Centrinė</td> 
+                        <td>Gričiupio g. 1</td> 
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Vakarinė</td> 
+                        <td>Karaliaus Mindaugo pr. 1</td> 
+                    </tr>
+                </table>
+                <div className="button-cluster">
+                    <button type="button">
+                        Ištrinti parduotuvę
+                    </button>
+                </div>
+            </div>
+            
+            
+            
         </div>
+        
     )
+    
 }
 export default Store;
