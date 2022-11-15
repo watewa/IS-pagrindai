6c8d906a0567d3d9f205d4be0cd33d18b6d84587
vejspa <v.spakauskas@gmail.com>
2022-11-15 22:01:52 +0200

    3

---
+++ src/pages/Stores/NewStore.tsx
@@ -0,0 +1,33 @@
+import {
+    useNavigate
+} from 'react-router-dom';
+
+const NewStorePage = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="box">
+            <h2>Nauja parduotuvė</h2>
+            <form method="post">
+                <div className="grid form center">
+                    <label htmlFor="name">Vardas</label>
+                    <input type="text" name="name" id="" />
+
+                    <label htmlFor="surname">Pavardė</label>
+                    <input type="text" name="surname" id="" />
+
+                    <label htmlFor="date">Gimimo data</label>
+                    <input type="date" name="date" id="" />
+
+                    <label htmlFor="email">El. paštas</label>
+                    <input type="email" name="email" id="" />
+
+                    <label htmlFor="address">Adresas</label>
+                    <input type="text" name="address" id="" />
+                </div>
+                <button className="button" onClick={() => navigate("/workerlist")}>Pateikti</button>
+            </form>
+        </div>
+    )
+}
+export default NewStorePage;
