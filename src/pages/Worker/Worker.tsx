db215163f739d049e1fb8f7cce80d3a01e64bb98
Zeptosec <armandasmatkevicius@gmail.com>
2022-12-04 19:09:37 +0200

    Added schedule

--- src/pages/Worker/Worker.tsx
+++ src/pages/Worker/Worker.tsx
@@ -9,6 +9,8 @@ const Worker = () => {
     const [worker, setWorker] = useState<any>({});
     const [isLoading, setIsLoading] = useState(true);
     const [errors, setErrors] = useState<Array<string>>([]);
+    const [tasks, setTasks] = useState<Array<any>>([]);
+    const [isFetchingTasks, setIsFetchingTasks] = useState(true);
     const { user }: any = useAuthContext();
 
     useEffect(() => {
@@ -20,7 +22,7 @@ const Worker = () => {
                     }
                 });
                 const json = await res.json();
-                if(!json.worker){
+                if (!json.worker) {
                     throw Error("Darbuotojo duomenys nerasti");
                 }
                 setWorker(json.worker);
@@ -32,8 +34,31 @@ const Worker = () => {
                 setErrors(w => [...w, err.message]);
             }
         }
+        const fetchTasks = async () => {
+            try {
+                setIsFetchingTasks(true);
+                const res = await fetch(`${process.env.REACT_APP_APIURL}/api/workers/schedule`, {
+                    headers: {
+                        'Authorization': `Bearer ${user.token}`
+                    }
+                });
+                const json = await res.json();
+                console.log(json.uzd);
+                setTasks(json.uzd);
+            } catch (err: any) {
+                setErrors(w => [...w, err.message]);
+            } finally {
+                setIsFetchingTasks(false);
+            }
+        }
+
         fetchWorker();
+        fetchTasks();
     }, [user, id])
+
+    const getTime = (val : string) => {
+        return val.split('T')[1].slice(0, 5);
+    }
 
     return (
         <div className="box">
@@ -41,31 +66,28 @@ const Worker = () => {
                 <h2 className="center">Sveiki sugrįže {worker.vardas} {worker.pavarde}!</h2>
                 <h3 className="center">Jūsų šiandienos tvarkaraštis:</h3>
                 <div>
-                    <table id="table">
-                        <tbody>
-                            <tr>
-                                <th>Nr.</th>
-                                <th>Pavadinimas</th>
-                                <th>Pradėti</th>
-                                <th>Trukmė (min)</th>
-                                <th>Paaiškinimas</th>
-                            </tr>
-                            <tr>
-                                <td>1</td>
-                                <td>Vazonai</td>
-                                <td>12:25</td>
-                                <td>30</td>
-                                <td>Paimti vazonus ir juos išvalyti.</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Tulpes</td>
-                                <td>13:00</td>
-                                <td>14</td>
-                                <td>Palaistyti visas tulpes.</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    {isFetchingTasks ? <h2>Kraunama...</h2> : <>
+                        <table id="table">
+                            <tbody>
+                                <tr>
+                                    <th>Nr.</th>
+                                    <th>Pavadinimas</th>
+                                    <th>Pradėti</th>
+                                    <th>Trukmė (min)</th>
+                                    <th>Paaiškinimas</th>
+                                </tr>
+                                {tasks.map((w, ind) => 
+                                    <tr key={ind}>
+                                        <td>{ind+1}</td>
+                                        <td>{w.pavadinimas}</td>
+                                        <td>{getTime(w.pradzios_laikas)}</td>
+                                        <td>{w.trukme}</td>
+                                        <td>{w.paaiskinimas}</td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+                    </>}
                 </div>
             </>}
             {errors.length > 0 ? <div className="error">{errors.map((w, ind) => (
